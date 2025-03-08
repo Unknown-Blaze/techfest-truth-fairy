e3ab982dc1c6f948deb66a5552b97507b21cee2b
Rudra Prasadh Ganesh <grudraprasadh@gmail.com>
2025-03-08 23:47:41 +0800

    tragic end

--- extension/frontend/contentScript.js
+++ extension/frontend/contentScript.js
@@ -16,7 +16,7 @@ const showFlagButton = (x, y) => {
     if (!flagButton) {
         flagButton = document.createElement('button');
         flagButton.id = 'flag-text-button';
-        flagButton.innerHTML = '🚩 Flag';
+        flagButton.innerHTML = '➕ Add';
         flagButton.style.position = 'absolute';
         flagButton.style.zIndex = '10000';
         flagButton.style.marginRight = '5px';
